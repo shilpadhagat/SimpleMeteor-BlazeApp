69408ca5a86de72ee0434e3c888623ced8268cd7
shilpadhagat <shilpa.dhagat15@gmail.com>
2016-12-27 14:46:23 -0500

    Add files via upload

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,26 @@
 Meteor.publish('frameworks', function() {
   return Frameworks.find();
 });
+
+Meteor.publish( 'frameworks', function( search ) {
+  check( search, Match.OneOf( String, null, undefined ) );
+
+  let query      = {},
+      projection = { limit: 10, sort: { name: 1 } };
+
+  if ( search ) {
+    let regex = new RegExp( search, 'i' );
+
+    query = {
+      $or: [
+        { name: regex },
+        { type: regex },
+        { description: regex }
+      ]
+    };
+
+    projection.limit = 100;
+  }
+
+  return Frameworks.find( query, projection );
+});
